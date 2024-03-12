456a3ae6bc5709cbc553e2f5d7496d1917620a17
Ben <kwalijkje@live.com>
2024-03-12 14:09:28 +0100

    Update luhn_algorithm.go

--- workspace/luhn_algorithm/luhn_algorithm.go
+++ workspace/luhn_algorithm/luhn_algorithm.go
@@ -1,11 +1,15 @@
 package luhnalgorithm
 
+// Takes an array of integers in the same order as the credit
+// card number they come from. Include all digits.
+// Returns a bool confirming if the Luhn Algorithm validated
+// the digits.
 func isValid(cardNumber []int) bool {
-	// cardNumber is array of digits from the card number, minus the last digit
 	sum := 0
 	parity := len(cardNumber) % 2
-	for i := 0; i < len(cardNumber); i++ {
-		if i%2 != parity {
+	for i := 0; i < (len(cardNumber) - 1); i++ {
+		// fmt.Print(cardNumber[i], "\n")
+		if (i%2) != parity {
 			sum += cardNumber[i]
 		} else if cardNumber[i] > 4 {
 			sum += (2*cardNumber[i] - 9)
@@ -15,3 +19,4 @@ func isValid(cardNumber []int) bool {
 	}
 	return cardNumber[len(cardNumber)-1] == (10 - (sum % 10))
 }
+
