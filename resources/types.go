7bfc655a327a281fb0e9a1ffd8fb96c3cc9c73ff
Ben <kwalijkje@live.com>
2024-03-25 09:59:47 +0100

    Add struct types to go file in resources

---
+++ resources/types.go
@@ -0,0 +1,22 @@
+package cctypes
+
+type ResourceTypeIssuer struct {
+	Numbers    []int  `json:"numbers"`
+	Length     int    `json:"length"`
+	Validation string `json:"string"`
+}
+
+type IncomingCardNumber struct {
+	CardNumber *string `json:"cardNumber"`
+	CVVNumber  *string `json:"cvvNumber"`
+}
+
+type OutgoingIssuerData struct {
+	MajorIndustry string `json:"majorIndustry"`
+	CardIssuer    string `json:"cardIssuer"`
+}
+
+type OutgoingCardNumber struct {
+	CardNumber *bool `json:"cardNumber"`
+	CVVNumber  *bool `json:"cvvNumber"`
+}
